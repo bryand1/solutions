478ba8318df27250aa7a848878827bf601c1826e
Bryan Andrade <bryand1@gmail.com>
2018-08-31 06:09:17 -0700

    LeetCode: 121. Best Time to Buy and Sell Stock C++

---
+++ leetcode/121. Best Time to Buy and Sell Stock/C++/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0, maxprofit = 0;
+        for (unsigned int i = 1; i < prices.size(); i++) {
+            profit += prices[i] - prices[i - 1];
+            if (profit < 0) profit = 0;
+            if (profit > maxprofit) maxprofit = profit;
+        }
+        return maxprofit;
+    }
+};
