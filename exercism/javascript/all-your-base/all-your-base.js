d0e489382a2208d5ca89a9fd8de177ff768c222e
Bryan Andrade <bryand1@gmail.com>
2018-11-22 00:21:25 -0800

    fix(exercism): javascript track - all your base

--- exercism/javascript/all-your-base/all-your-base.js
+++ exercism/javascript/all-your-base/all-your-base.js
@@ -1,15 +1,30 @@
+const validInputBase = n => Number.isInteger(n) && n > 1;
+const validOutputBase = n => Number.isInteger(n) && n > 1;
+const validSequence = (seq, inputBase) => {
+  const noLeadingZero = seq.length === 1 || (seq.length > 1 && seq[0] !== 0);
+  const outOfRange = seq.filter(x => x < 0 || x >= inputBase).length;
+  return noLeadingZero && outOfRange === 0;
+}
+
 export const convert = (seq, inputBase, outputBase) => {
-  if (inputBase === undefined || inputBase < 2 || !Number.isInteger(inputBase)) throw Error('Wrong input base');
-  if (outputBase === undefined || outputBase < 2 || !Number.isInteger(outputBase)) throw Error('Wrong output base');
-  if (seq.length === 0 || (seq.length > 1 && seq[0] === 0)) throw Error('Input has wrong format');
-  if (seq.filter(x => x < 0 || x >= inputBase).length) throw new Error('Input has wrong format');
+  // Validate arguments
+  if (!validInputBase(inputBase)) throw Error('Wrong input base');
+  if (!validOutputBase(outputBase)) throw Error('Wrong output base');
+  if (!validSequence(seq, inputBase)) throw Error('Input has wrong format');
+
+  // Convert input sequence to base 10 integer
   let length = seq.length - 1;
-  const baseTen = seq.map((i) => {
+  let baseTen = seq.map((i) => {
     const x = i * (inputBase ** length);
     length -= 1;
     return x;
   }).reduce((a, b) => a + b);
-  const baseTenSeq = baseTen.toString().split('').map(x => Number(x));
-  length = baseTenSeq.length - 1;
 
+  // Convert base 10 integer to sequence of `outputBase` integer(s)
+  const out = [];
+  do {
+    out.unshift(baseTen % outputBase);
+    baseTen = Math.floor(baseTen / outputBase);
+  } while (baseTen);
+  return out;
 };
--- exercism/javascript/all-your-base/all-your-base.spec.js
+++ exercism/javascript/all-your-base/all-your-base.spec.js
@@ -5,31 +5,31 @@ describe('Converter', () => {
     expect(convert([1], 2, 10)).toEqual([1]);
   });
 
-  xtest('binary to single decimal', () => {
+  test('binary to single decimal', () => {
     expect(convert([1, 0, 1], 2, 10)).toEqual([5]);
   });
 
-  xtest('single decimal to binary', () => {
+  test('single decimal to binary', () => {
     expect(convert([5], 10, 2)).toEqual([1, 0, 1]);
   });
 
-  xtest('binary to multiple decimal', () => {
+  test('binary to multiple decimal', () => {
     expect(convert([1, 0, 1, 0, 1, 0], 2, 10)).toEqual([4, 2]);
   });
 
-  xtest('decimal to binary', () => {
+  test('decimal to binary', () => {
     expect(convert([4, 2], 10, 2)).toEqual([1, 0, 1, 0, 1, 0]);
   });
 
-  xtest('trinary to hexadecimal', () => {
+  test('trinary to hexadecimal', () => {
     expect(convert([1, 1, 2, 0], 3, 16)).toEqual([2, 10]);
   });
 
-  xtest('hexadecimal to trinary', () => {
+  test('hexadecimal to trinary', () => {
     expect(convert([2, 10], 16, 3)).toEqual([1, 1, 2, 0]);
   });
 
-  xtest('15-bit integer', () => {
+  test('15-bit integer', () => {
     expect(convert([3, 46, 60], 97, 73)).toEqual([6, 10, 45]);
   });
 
@@ -39,7 +39,7 @@ describe('Converter', () => {
     }).toThrow(new Error('Input has wrong format'));
   });
 
-  xtest('single zero', () => {
+  test('single zero', () => {
     expect(convert([0], 10, 2)).toEqual([0]);
   });
 
