ca315ab2f45bfb2609acd411944b7a526384cd8e
Bryan Andrade <bryand1@gmail.com>
2018-11-22 12:45:24 -0800

    feat(exercism): javascript track - allergies

---
+++ exercism/javascript/allergies/allergies.js
@@ -0,0 +1,27 @@
+const codes = {
+    1: 'eggs',
+    2: 'peanuts',
+    4: 'shellfish',
+    8: 'strawberries',
+    16: 'tomatoes',
+    32: 'chocolate',
+    64: 'pollen',
+    128: 'cats',
+};
+
+export default class Allergies {
+  constructor(code) {
+    this.allergies = [];
+    for (let k in codes) {
+      if (code & k) this.allergies.push(codes[k]);
+    }
+  }
+
+  list() {
+    return this.allergies;
+  }
+
+  allergicTo(food) {
+    return this.allergies.includes(food);
+  }
+}
