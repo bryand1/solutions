a948e71e8ecfbaee0a111a8310e99a7af831a3c6
Bryan Andrade <bryand1@gmail.com>
2019-02-26 07:58:28 -0800

    fix(leetcode): 69. Sqrt(x)

--- leetcode/69. Sqrt(x)/solution.cpp
+++ leetcode/69. Sqrt(x)/solution.cpp
@@ -5,23 +5,18 @@ using namespace std;
 class Solution {
 public:
     int mySqrt(int x) {
-        int l = 0;
-        int r = x;
-        int m;
+        long l = 0, r = (long)x, t = (long)x, m;
         while (l <= r) {
             m = l + (r - l) / 2;
-            if ((m * m <= x) && (x < ((m + 1) * (m + 1)))) {
-                cout << m << endl;
-                return m;
-            } else if (x < (m * m)) {
-                cout << m << endl;
+            if ((m * m <= t) && (t < ((m + 1) * (m + 1)))) {
+                return (int) m;
+            } else if (t < (m * m)) {
                 r = m;
             } else {
-                cout << m << endl;
                 l = m + 1;
             }
         }
-        return m;
+        return (int) m;
     }
 };
 
