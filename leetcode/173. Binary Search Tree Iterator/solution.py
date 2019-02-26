390af7a4966e6d77f02b0d178443193d0081540a
Bryan Andrade <bryand1@gmail.com>
2019-02-26 09:39:56 -0800

    173. Binary Search Tree Iterator
    LeetCode Python3 Third Attempt using deque

--- leetcode/173. Binary Search Tree Iterator/solution.py
+++ leetcode/173. Binary Search Tree Iterator/solution.py
@@ -4,34 +4,33 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
+from collections import deque
+
 
 class BSTIterator:
 
     def __init__(self, root: TreeNode):
-        self.i = 0
-        self.sorted = []
+        self.q = deque()
         if root:
             self.inorder(root)
 
     def inorder(self, node):
         if node:
             self.inorder(node.left)
-            self.sorted.append(node.val)
+            self.q.append(node.val)
             self.inorder(node.right)
 
     def next(self) -> int:
         """
         @return the next smallest number
         """
-        val = self.sorted[self.i]
-        self.i += 1
-        return val
+        return self.q.popleft()
 
     def hasNext(self) -> bool:
         """
         @return whether we have a next smallest number
         """
-        return self.i < len(self.sorted)
+        return len(self.q) > 0
 
 # Your BSTIterator object will be instantiated and called as such:
 # obj = BSTIterator(root)
