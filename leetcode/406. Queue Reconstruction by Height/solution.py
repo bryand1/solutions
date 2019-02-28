e69dcdfdfa5181de5dbaeab4bd3d0fa519384d09
Bryan Andrade <bryand1@gmail.com>
2019-02-28 09:01:05 -0800

    406. Queue Reconstruction by Height
    Clean up algorithm

--- leetcode/406. Queue Reconstruction by Height/solution.py
+++ leetcode/406. Queue Reconstruction by Height/solution.py
@@ -1,11 +1,10 @@
 class Solution:
     def reconstructQueue(self, people: 'List[List[int]]') -> 'List[List[int]]':
-        people.sort(key=lambda p: (p[0], -p[1]), reverse=True)
-        ret = []
+        people.sort(key=lambda (h, k): (-h, -k))
+        queue = []
         for person in people:
-            h, k = person
-            ret.insert(k, person)
-        return ret
+            ret.insert(person[1], person)
+        return queue
 
 if __name__ == '__main__':
     input = [[7,0],[4,4],[7,1],[5,0],[6,1],[5,2]]
