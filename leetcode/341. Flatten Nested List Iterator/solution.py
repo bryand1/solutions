ab1729691e6a52ff0bcc01e929e9b593ce2ecbe9
Bryan Andrade <bryand1@gmail.com>
2019-02-28 12:16:37 -0800

    341. Flatten Nested List Iterator

--- leetcode/341. Flatten Nested List Iterator/solution.py
+++ leetcode/341. Flatten Nested List Iterator/solution.py
@@ -30,32 +30,36 @@ class NestedIterator(object):
         Initialize your data structure here.
         :type nestedList: List[NestedInteger]
         """
-        res = []
-        self.flatten(nestedList, res)
-        self.i = 0
-        self.n = len(res)
-        self.it = iter(res)
-
-    def flatten(self, nestedList, res):
-        for nestedInteger in nestedList:
-            if nestedInteger.isInteger():
-                res.append(nestedInteger.getInteger())
-            else:
-                self.flatten(nestedInteger.getList(), res)
+        self.data = nestedList
+        self.cur = -1
+        self.inneriter = None
 
     def next(self):
         """
         :rtype: int
         """
-        self.i += 1
-        return next(self.it)
+        if self.inneriter:
+            return self.inneriter.next()
+        return self.data[self.cur].getInteger()
 
     def hasNext(self):
         """
         :rtype: bool
         """
-        return self.i < self.n
-        
+        if self.inneriter and self.inneriter.hasNext():
+            return True
+        self.inneriter = None
+        if self.cur < len(self.data) - 1:
+            self.cur += 1
+            if self.data[self.cur].isInteger():
+                return True
+            self.inneriter = NestedIterator(self.data[self.cur].getList())
+            if self.inneriter.hasNext():
+                return True
+            self.inneriter = None
+            return self.hasNext()
+        return False
+
 
 # Your NestedIterator object will be instantiated and called as such:
 # i, v = NestedIterator(nestedList), []
